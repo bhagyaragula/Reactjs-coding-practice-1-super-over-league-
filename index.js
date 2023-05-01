9b557c05d025cbd804d03a5de4d76a93b87917b4
bhagya <gvrajreddy@gmail.com>
2023-05-01 18:20:52 +0000

    Reactjs coding practice1 super over league

---
+++ index.js
@@ -0,0 +1,16 @@
+const element = (
+  //   Write your code here.
+  <div className="bgContainer">
+    <h1 className="heading">Super Over League</h1>
+    <img
+      className="images"
+      src="https://assets.ccbp.in/frontend/react-js/rcb-img.png"
+    />
+    <img
+      className="images"
+      src="https://assets.ccbp.in/frontend/react-js/csk-img.png"
+    />
+  </div>
+);
+
+ReactDOM.render(element, document.getElementById("root"));
